9c8d0cfaa0c69003d98fb1058ea73dc9d6cd50da
Brian Munzenberger <brian@munzenberger.com>
2018-12-22 18:17:39 -0500

    Solve day 1

--- day01/src/main/kotlin/advent/Main.kt
+++ day01/src/main/kotlin/advent/Main.kt
@@ -1,5 +1,41 @@
 package advent
 
+import java.io.File
+
 fun main(args:Array<String>) {
 
+    val input = File(ClassLoader.getSystemResource("input.txt").file).readLines().map { it.toInt() }
+
+    println(solvePart1(input))
+    println(solvePart2(input))
+}
+
+fun solvePart1(input: List<Int>) = input.fold(0) { freq, delta -> freq + delta }
+
+fun solvePart2(input: List<Int>): Int {
+
+    val repeat = Repeat(input)
+    val history = mutableMapOf<Int,Int>()
+    var freq = repeat.next()
+
+    while (!history.contains(freq)) {
+        history[freq] = freq
+        freq += repeat.next()
+    }
+
+    return freq
+}
+
+class Repeat<T>(private val items: List<T>) {
+
+    private var index = 0
+
+    fun next(): T {
+        val value = items[index]
+        index += 1
+        if (index == items.size) {
+            index = 0
+        }
+        return value
+    }
 }
