14a3e2d86ee01e2faadb1f187023bf850facc13c
Brian Munzenberger <brian@munzenberger.com>
2018-12-24 13:32:25 -0500

    Solve day 5

--- day05/src/main/kotlin/advent/Main.kt
+++ day05/src/main/kotlin/advent/Main.kt
@@ -1,5 +1,48 @@
 package advent
 
+import java.io.File
+import kotlin.math.min
+
 fun main(args:Array<String>) {
 
+    val input = File(ClassLoader.getSystemResource("input.txt").file).readText()
+
+    println(solvePart1(input))
+    println(solvePart2(input))
+}
+
+fun solvePart1(polymer: String) = react(polymer)
+
+fun solvePart2(polymer: String): Int {
+
+    var solution = Int.MAX_VALUE
+
+    val allChars = polymer.map { it.toLowerCase() }.distinct()
+
+    allChars.forEach { c ->
+        polymer.filterNot { it.toLowerCase() == c }.run {
+            solution = min(solution, react(this))
+        }
+    }
+
+    return solution
+}
+
+fun react(polymer: String): Int {
+
+    val p = StringBuffer(polymer)
+
+    var i = 0
+
+    while (i < p.length-1) {
+        val (x, y) = p[i] to p[i+1]
+        if (x != y && x.toUpperCase() == y.toUpperCase()) {
+            p.delete(i, i+2)
+            i = when (i) { 0 -> 0 else -> i-1 }
+        } else {
+            i++
+        }
+    }
+
+    return p.length
 }
